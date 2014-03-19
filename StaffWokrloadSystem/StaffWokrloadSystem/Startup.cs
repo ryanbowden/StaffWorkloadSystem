dd48f477f7f2cd03a08a1018ae36f99bae388c64
Ryan Bowden <ryanbowden1@outlook.com>
2014-03-19 17:33:38 +0000

    Adding Basic Website

---
+++ StaffWokrloadSystem/StaffWokrloadSystem/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StaffWokrloadSystem.Startup))]
+namespace StaffWokrloadSystem
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
