85c5883828b620dfd72119a78d1b391d9111b1d2
Ryan Bowden <ryanbowden1@outlook.com>
2014-03-20 20:33:24 +0000

    Working Website With Database connection

---
+++ StaffWorkloadSystem/StaffWorkloadSystem/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StaffWorkloadSystem.Startup))]
+namespace StaffWorkloadSystem
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
