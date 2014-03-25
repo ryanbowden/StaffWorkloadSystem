d57be4584068ea1319edbf4a0a081f3500584c74
Ryan Bowden <ryanbowden1@outlook.com>
2014-03-25 10:59:53 +0000

    Staff to Duties Connection
    Completed the section to connect staff to duties.

--- StaffWorkloadSystem/StaffWorkloadSystem/StaffDutyConnection.aspx
+++ StaffWorkloadSystem/StaffWorkloadSystem/StaffDutyConnection.aspx
@@ -1,3 +1,35 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="StaffDutyConnection.aspx.cs" Inherits="StaffWorkloadSystem.StaffDutyConnection" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>
+        Staff to Duty Connection!
+    </h1>
+    <p>
+        Here you are able to connect a staff member to a duty and the hours it will take for the year.</p>
+    <p>
+        <asp:Label ID="lblError" runat="server"></asp:Label>
+    </p>
+    <p>
+        Staff Name:
+        <asp:DropDownList ID="StaffID" runat="server" DataSourceID="SqlData_Staff" DataTextField="Name" DataValueField="ID">
+        </asp:DropDownList>
+        <asp:SqlDataSource ID="SqlData_Staff" runat="server" ConnectionString="<%$ ConnectionStrings:staffwoAJ4TKlNRsConnectionString %>" SelectCommand="SELECT [Name], [ID] FROM [StaffDetails]"></asp:SqlDataSource>
+    </p>
+    <p>
+        Duty :
+        <asp:DropDownList ID="DutyID" runat="server" DataSourceID="SqlData_Duties" DataTextField="Name" DataValueField="ID">
+        </asp:DropDownList>
+        <asp:SqlDataSource ID="SqlData_Duties" runat="server" ConnectionString="<%$ ConnectionStrings:staffwoAJ4TKlNRsConnectionString %>" SelectCommand="SELECT [ID], [Name] FROM [Duties]"></asp:SqlDataSource>
+    </p>
+    <p>
+        Hours :
+        <asp:TextBox ID="Hours" runat="server"></asp:TextBox><asp:RequiredFieldValidator runat="server"
+      id="regDesc" ControlToValidate="Hours"
+      ErrorMessage = "You must Provide the hours this duty will take the staff member over the year.!"
+      display="Dynamic" />
+        <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" BorderStyle="None" ControlToValidate="Hours" ErrorMessage="Please only enter Numbers" ForeColor="#FF3300" ValidationExpression="^\d+$"></asp:RegularExpressionValidator>
+    </p>
+    <p>
+        <asp:Button ID="CompleteConnection" runat="server" OnClick="CompleteConnection_Click" Text="Connect" />
+    </p>
+
 </asp:Content>
--- StaffWorkloadSystem/StaffWorkloadSystem/StaffDutyConnection.aspx.cs
+++ StaffWorkloadSystem/StaffWorkloadSystem/StaffDutyConnection.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
 
 namespace StaffWorkloadSystem
 {
@@ -12,6 +15,59 @@ namespace StaffWorkloadSystem
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void CompleteConnection_Click(object sender, EventArgs e)
+        {
+            //first check that all sections are filled in
+            if (Hours.Text == null)
+            {
+                //Not all things added, Post a message
+                lblError.Text = "A Field Was left empty :(";
+            }
+            else
+            {
+                //everything is filled in so now need to add it to the database.
+                
+                //need to convert all the strings to ints
+                int _StaffID = Convert.ToInt32(StaffID.SelectedValue);
+                int _DutyId = Convert.ToInt32(DutyID.SelectedValue);
+                int _hours = Convert.ToInt32(Hours.Text);
+
+
+
+                //Now need to connect to the database
+                //try
+                //{
+                    string ConnectionString = "Data Source=y2x8tstejy.database.windows.net;Initial Catalog=staffwoAJ4TKlNRs;User ID=RyanBowden;Password=Molly_10";
+                    string Query = "INSERT INTO [StaffDuties] (DutyID,StaffID,Hours) values (@duty,@staff,@hours)";
+                    using (SqlConnection conn = new SqlConnection(ConnectionString))
+                    {
+                        using (SqlCommand comm = new SqlCommand())
+                        {
+                            comm.Connection = conn;
+                            comm.CommandType = CommandType.Text;
+                            comm.CommandText = Query;
+                            comm.Parameters.AddWithValue("@duty", _StaffID);
+                            comm.Parameters.AddWithValue("@staff", _DutyId);
+                            comm.Parameters.AddWithValue("@hours", _hours);
+                            //try
+                            //{
+                                conn.Open();
+                                comm.ExecuteNonQuery();
+                            //}
+                            //catch
+                            //{
+                            //    lblError.Text = "An issue has happened when connecting to the database";
+                            //}
+                        }
+                    }
+                //}
+                //catch
+                //{
+
+                //}
+            }
         }
     }
 }
