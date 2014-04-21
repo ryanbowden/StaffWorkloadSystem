0aa520f59ce27f28c54f82b2520ae25b4b923290
Ryan Bowden <ryanbowden1@outlook.com>
2014-04-21 22:07:56 +0100

    Module staff system started

--- StaffWorkloadSystem/StaffWorkloadSystem/ModuleStaff.aspx
+++ StaffWorkloadSystem/StaffWorkloadSystem/ModuleStaff.aspx
@@ -5,9 +5,14 @@
             <h2>Sorry there been a error</h2>
         </AnonymousTemplate>
         <LoggedInTemplate>
-            <h1 runat="server">Module - </h1>
-
-            <asp:Label ID="lblError" runat="server" Text=""></asp:Label>
+            <h1 runat="server">Module - Add Staff</h1>
+            <h2>Module Details</h2>
+            <label>Module Name -&nbsp</label><asp:Label ID="ModuleName" runat="server" Text=""></asp:Label>
+            <label>  |  Module Code -&nbsp</label><asp:Label ID="ModuleCode" runat="server" Text=""></asp:Label>
+            <h2>Add staff</h2>
+            <asp:Label ID="lblError" runat="server" Text="" ForeColor="Red" Font-Underline="True" Font-Bold="True" Font-Size="Large"></asp:Label>
+            <h2>Current Staff</h2>
+            
         </LoggedInTemplate>
     </asp:LoginView>
 </asp:Content>
--- StaffWorkloadSystem/StaffWorkloadSystem/ModuleStaff.aspx.cs
+++ StaffWorkloadSystem/StaffWorkloadSystem/ModuleStaff.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -15,7 +16,7 @@ namespace StaffWorkloadSystem
             if(!IsPostBack)
             {
                 //Check whether Module Id has been sent
-                if(Request.QueryString["ID"] ==  "")
+                if(Request.QueryString["ID"] !=  "")
                 {
                     //Lets save the varible 
                     int ModuleID = Convert.ToInt32(Request.QueryString["ID"]);
@@ -36,7 +37,55 @@ namespace StaffWorkloadSystem
         public void GetModuleDetails(int ID)
         {
             //Need to get the details of this Module and Display it on the Page
+            //Query to get data
+            string ModuleDetailsQuery = "SELECT * FROM Modules WHERE ID="+ID;
+            //Connection string
+            string connstr = "Data Source=y2x8tstejy.database.windows.net;Initial Catalog=staffwoAJ4TKlNRs;User ID=RyanBowden;Password=Molly_10";
+            SqlConnection ModuleSelectConn = new SqlConnection(connstr);
 
+            try
+            {
+                //Set the command up
+                SqlCommand cmd = new SqlCommand(ModuleDetailsQuery, ModuleSelectConn);
+                //open reader
+                SqlDataReader reader;
+                //Open Connection
+                ModuleSelectConn.Open();
+                //Execute reader
+                reader = cmd.ExecuteReader();
+                //open reader
+                reader.Read();
+                //get the Data that's needed
+                string ModuleName = reader["Name"].ToString();
+                string ModuleCode = reader["ModuleCode"].ToString();
+                //Close Reader
+                reader.Close();
+                ((Label)LoginView1.FindControl("ModuleName")).Text = ModuleName;
+                ((Label)LoginView1.FindControl("ModuleCode")).Text = ModuleCode;
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                //Close the Connections
+                ModuleSelectConn.Close();
+
+
+            }
+        }
+
+        public void GetModuleStaff(int ModuleID)
+        {
+            //Will be used to get the staff that are part of the module
+        }
+
+        public bool AddStaffToModule()
+        {
+            //To add staff member to the module
+            return true;
         }
     }
 }
