91bd6599acb8169f9a326b9b1e8015af4a7e6251
Ryan Bowden <ryanbowden1@outlook.com>
2014-04-22 17:42:42 +0100

    Able to add staff to Modules

--- StaffWorkloadSystem/StaffWorkloadSystem/ModuleStaff.aspx
+++ StaffWorkloadSystem/StaffWorkloadSystem/ModuleStaff.aspx
@@ -1,18 +1,37 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ModuleStaff.aspx.cs" Inherits="StaffWorkloadSystem.ModuleStaff" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-    <asp:LoginView id="LoginView1" runat="server" ViewStateMode="Disabled">
+    <asp:LoginView id="LoginView1" runat="server" ViewStateMode="Enabled">
         <AnonymousTemplate>
             <h2>Sorry there been a error</h2>
         </AnonymousTemplate>
         <LoggedInTemplate>
             <h1 runat="server">Module - Add Staff</h1>
+
+
             <h2>Module Details</h2>
             <label>Module Name -&nbsp</label><asp:Label ID="ModuleName" runat="server" Text=""></asp:Label>
             <label>  |  Module Code -&nbsp</label><asp:Label ID="ModuleCode" runat="server" Text=""></asp:Label>
+
+
             <h2>Add staff</h2>
-            <asp:Label ID="lblError" runat="server" Text="" ForeColor="Red" Font-Underline="True" Font-Bold="True" Font-Size="Large"></asp:Label>
+            <p>Add a staff Member to this Module</p>
+            <p><asp:Label ID="lblStatus" runat="server" Text="" oreColor="Red" Font-Underline="True" Font-Bold="True" Font-Size="Large"></asp:Label></p>
+            <p><asp:Label ID="lblError" runat="server" Text="" ForeColor="Red" Font-Underline="True" Font-Bold="True" Font-Size="Large"></asp:Label></p>
+            <Label>Staff member: </Label>
+            <asp:DropDownList ID="ddl_StaffMember" runat="server" DataSourceID="SqlDataSource1" DataTextField="Name" DataValueField="ID"></asp:DropDownList>
+            <label>Weighting: </label> 
+            <asp:TextBox ID="txtbox_Weighting" runat="server"></asp:TextBox>
+
+
+            <label>Coordinator: </label>
+            <asp:DropDownList ID="ddl_Coordinator" runat="server">
+                <asp:ListItem Value="1">True</asp:ListItem>
+                <asp:ListItem Selected="True" Value="0">False</asp:ListItem>
+            </asp:DropDownList>
+            <asp:Button ID="but_SubmitStaff" runat="server" Text="Add staff member to module" OnClick="but_SubmitStaff_Click" />
             <h2>Current Staff</h2>
             
         </LoggedInTemplate>
     </asp:LoginView>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:staffwoAJ4TKlNRsConnectionString %>" SelectCommand="SELECT [ID], [Name] FROM [StaffDetails]"></asp:SqlDataSource>
 </asp:Content>
--- StaffWorkloadSystem/StaffWorkloadSystem/ModuleStaff.aspx.cs
+++ StaffWorkloadSystem/StaffWorkloadSystem/ModuleStaff.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,8 @@ namespace StaffWorkloadSystem
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Clear any Labels
+            ((Label)LoginView1.FindControl("lblError")).Text = "";
             //Check Weather it is a ispostback
             if(!IsPostBack)
             {
@@ -23,15 +26,12 @@ namespace StaffWorkloadSystem
                     //Now need to start setting up the page.
                     //First need to get this module details 
                     GetModuleDetails(ModuleID);
-
                 }
                 else
                 {
                     ((Label)LoginView1.FindControl("lblError")).Text = "Something Has gone wrong";
                 }
             }
-
-            
         }
 
         public void GetModuleDetails(int ID)
@@ -72,8 +72,6 @@ namespace StaffWorkloadSystem
             {
                 //Close the Connections
                 ModuleSelectConn.Close();
-
-
             }
         }
 
@@ -82,9 +80,141 @@ namespace StaffWorkloadSystem
             //Will be used to get the staff that are part of the module
         }
 
-        public bool AddStaffToModule()
+        public bool AddStaffToModule(int StaffID, int ModuleID, int Coordinator, decimal Weighting)
         {
-            //To add staff member to the module
+            //connection string that will be required for connecting to the database
+            string ConnectionString = "Data Source=y2x8tstejy.database.windows.net;Initial Catalog=staffwoAJ4TKlNRs;User ID=RyanBowden;Password=Molly_10";
+            SqlConnection con = new SqlConnection(ConnectionString);
+            //Need to add it to the staffDuties database
+
+            //Now need to connect to the database
+            try
+            {
+                string Query = "INSERT INTO [StaffModules] (ModuleID,StaffID,Weighting,Coordinator) values (@Module,@staff,@Weighting,@Coordinator)";
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    using (SqlCommand comm = new SqlCommand())
+                    {
+                        comm.Connection = conn;
+                        comm.CommandType = CommandType.Text;
+                        comm.CommandText = Query;
+                        comm.Parameters.AddWithValue("@Module", ModuleID);
+                        comm.Parameters.AddWithValue("@staff", StaffID);
+                        comm.Parameters.AddWithValue("@Weighting", Weighting);
+                        comm.Parameters.AddWithValue("@Coordinator", Coordinator);
+                        try
+                        {
+                            conn.Open();
+                            comm.ExecuteNonQuery();
+                        }
+                        catch
+                        {
+                            ((Label)LoginView1.FindControl("ModuleName")).Text = "An issue has happened when connecting to the database";
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return true;
+        }
+
+        protected void but_SubmitStaff_Click(object sender, EventArgs e)
+        {
+            //Clean labels 
+            ((Label)LoginView1.FindControl("lblStatus")).Text = "";
+            ((Label)LoginView1.FindControl("lblError")).Text = "";
+            //When a user Click this button we need to add the staff member to this module.
+            //First check everything has been passed through.
+            if ((((DropDownList)LoginView1.FindControl("ddl_Coordinator")).SelectedValue == "") || (((DropDownList)LoginView1.FindControl("ddl_StaffMember")).SelectedValue == ""))
+            {
+                ((Label)LoginView1.FindControl("lblError")).Text = "Something has gone wrong";
+                return;
+            }
+            int ModuleID = Convert.ToInt32(Request.QueryString["ID"]);
+            int Coordinator = Convert.ToInt32(((DropDownList)LoginView1.FindControl("ddl_Coordinator")).SelectedValue);
+            int StaffID = Convert.ToInt32(((DropDownList)LoginView1.FindControl("ddl_StaffMember")).SelectedValue);
+            //Now get the weighting
+            string oldWeighting = ((TextBox)LoginView1.FindControl("txtbox_Weighting")).Text.ToString();
+            //Now convert it.
+            decimal Weighting = 0.0M;
+            //now turn it to a decimal
+            if(!decimal.TryParse(oldWeighting, out Weighting))
+            {
+                //If it fails need to run this and stop the code
+                ((Label)LoginView1.FindControl("lblError")).Text = "Weight is in the Wrong Format! Needs to be 0.00 - 1. Staff workload weightings are usally 0.25/0.33/0.50/1 Please change yours correctly.";
+                //This will stop the function running
+                return;
+            }
+            //Now everything is check and working we can now start putting it into the database.
+            //First need to check that a staff member is not already part of this module.
+            bool StaffCheck = CheckStaffAlreadyOnModule(StaffID, ModuleID);
+            //now check it it worked.
+            if (StaffCheck == true)
+            {
+                //Module Not already added so need to add it then.
+                bool addstaff = AddStaffToModule(StaffID, ModuleID, Coordinator, Weighting);
+                if (addstaff == true)
+                {
+                    ((Label)LoginView1.FindControl("lblStatus")).Text = "Staff Member added sucessfully";
+                }
+                else
+                {
+                    ((Label)LoginView1.FindControl("lblError")).Text = "Something went wrong!";
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        protected bool CheckStaffAlreadyOnModule(int StaffID, int ModuleID)
+        {
+
+            //This will be used to check if a staff member is already part of that module
+            //connection string that will be required for connecting to the database
+            string ConnectionString = "Data Source=y2x8tstejy.database.windows.net;Initial Catalog=staffwoAJ4TKlNRs;User ID=RyanBowden;Password=Molly_10";
+            SqlConnection con = new SqlConnection(ConnectionString);
+            //now a query
+            // a simple select query will do here just to see if there one row
+            string _StaffDutiesQuery = "SELECT * FROM StaffModules ";
+            _StaffDutiesQuery += "WHERE StaffID='" + StaffID + "' AND ModuleID = '" + ModuleID + "'";
+            //Need to run the query and then put the reader onlin we will use the HASROWS feature to check
+            try
+            {
+                SqlCommand cmd = new SqlCommand(_StaffDutiesQuery, con);
+                SqlDataReader reader;
+                //open connection
+                con.Open();
+                //run the commmand
+                reader = cmd.ExecuteReader();
+                //Check for rows
+                if (reader.HasRows)
+                {
+                    //Data returns rows
+                    reader.Close();
+                    return false;
+                }
+                reader.Close();
+            }
+            catch
+            {
+                ((Label)LoginView1.FindControl("lblError")).Text = "When Checking Staff Modules something went wrong";
+                return false;
+            }
+            finally
+            {
+                //close the database connection
+                con.Close();
+            }
             return true;
         }
     }
